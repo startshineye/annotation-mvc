3ff3d5751301d7ec1625df4bd07b6fccc4947e32
startshineye <13001955858@163.com>
2017-01-15 19:00:51 +0800

    annotation-mvc

--- annotation-mvc/src/org/apache/mvc/bean/ActionBean.java
+++ annotation-mvc/src/org/apache/mvc/bean/ActionBean.java
@@ -1,5 +1,77 @@
 package org.apache.mvc.bean;
 
+import java.util.Arrays;
+
+import org.springframework.annotation.RequestMethod;
+
+/**
+ * Action对应的bean
+ * 
+ * @author yxm
+ * @date 2017-1-15
+ */
 public class ActionBean {
+	private String path;// 请求路径
+	private RequestMethod[] types;// 请求类型(post、get)
+	private Class[] params;// 方法参数集合
+	private String classPath;// 处理类路径
+	private String method;// 请求方法名称
+	private boolean isResponseBody;// 是否将内容作为响应正文输出
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public RequestMethod[] getTypes() {
+		return types;
+	}
+
+	public void setTypes(RequestMethod[] types) {
+		this.types = types;
+	}
+
+	public Class[] getParams() {
+		return params;
+	}
+
+	public void setParams(Class[] params) {
+		this.params = params;
+	}
+
+	public String getClassPath() {
+		return classPath;
+	}
+
+	public void setClassPath(String classPath) {
+		this.classPath = classPath;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public boolean isResponseBody() {
+		return isResponseBody;
+	}
+
+	public void setResponseBody(boolean isResponseBody) {
+		this.isResponseBody = isResponseBody;
+	}
+
+	@Override
+	public String toString() {
+		return "ActionBean [path=" + path + ", types=" + Arrays.toString(types)
+				+ ", params=" + Arrays.toString(params) + ", classPath="
+				+ classPath + ", method=" + method + ", isResponseBody="
+				+ isResponseBody + "]";
+	}
 
 }
--- annotation-mvc/src/org/apache/mvc/scanner/Scanner.java
+++ annotation-mvc/src/org/apache/mvc/scanner/Scanner.java
@@ -1,5 +1,99 @@
 package org.apache.mvc.scanner;
 
-public class Scanner {
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Map;
 
+import org.apache.mvc.bean.ActionBean;
+import org.springframework.annotation.Controller;
+import org.springframework.annotation.RequestMapper;
+import org.springframework.annotation.ResponseBody;
+
+/**
+ * 扫描器类
+ * @author yxm
+ * @date 2017-1-15
+ *
+ */
+public class Scanner {
+	/**
+	 * 扫描路径下所有文件
+	 * @param path
+	 * @param actionBeanMap
+	 * @return
+	 */
+  public static Map<String,ActionBean> scannerPackage(String path,Map<String,ActionBean> actionBeanMap) throws Exception{
+	  System.out.println("路径为:"+path);
+	  
+	  String packagePath = path.replace(".","/");
+	  URL url = Thread.currentThread().getContextClassLoader().getResource(packagePath);
+	  File file = new File(url.getFile());
+	  if(!file.exists()){
+		  throw new RuntimeException("路径错误!");
+	  }
+	  
+	  //目录
+	  if(file.isDirectory()){
+		  File[] files = file.listFiles();
+		  for (File fi: files) {
+			if(fi.isDirectory()){
+				scannerPackage(path + "." + fi.getName(), actionBeanMap);
+			}
+			if (fi.isFile()) {
+                scannerClass(path + "." + fi.getName(), actionBeanMap);
+            }
+		}
+	  }
+	  
+	  //文件
+	  if(file.isFile()){
+		 scannerClass(path+"."+file.getName(), actionBeanMap); 
+	  }
+	  
+	  return actionBeanMap;
+  }
+  
+  /**
+   * 扫描类中的所有方法
+   */
+  public static void scannerClass(String filePath,Map<String, ActionBean> actionBeanMap) throws Exception{
+	  String path = filePath;
+	  if(filePath.endsWith(".class")){
+		  path = filePath.substring(0,filePath.lastIndexOf(".class"));
+	  }
+	  
+	  Class clazz = Class.forName(path);
+	  
+	  //非controller注解类跳过
+	  if(!clazz.isAnnotationPresent(Controller.class)){
+		  return;
+	  }
+	  
+	  Method[] methods = clazz.getDeclaredMethods();
+	  for (Method method : methods) {
+		  if (method.isAnnotationPresent(RequestMapper.class)) {
+              ActionBean ab = scannerMethod(clazz, method);
+              actionBeanMap.put(ab.getPath(), ab);
+          }
+	}
+	  
+  }
+  
+  /**
+   * 得到方法参数及方法返回类型等并封装为ActionBean
+   */
+  public static ActionBean scannerMethod(Class cc, Method method){
+	      RequestMapper requestMapper = method.getAnnotation(RequestMapper.class);
+	      Controller controller = (Controller)cc.getAnnotation(Controller.class);
+	      
+	      ActionBean actionBean = new ActionBean();
+	      actionBean.setClassPath(cc.getName());
+	      actionBean.setMethod(method.getName());
+	      actionBean.setTypes(requestMapper.method());
+	      actionBean.setPath(controller.value() + requestMapper.value());
+	      actionBean.setParams(method.getParameterTypes());
+	      actionBean.setResponseBody(method.isAnnotationPresent(ResponseBody.class));
+          return actionBean;
+  }
 }
